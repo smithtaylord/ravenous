b13601e546703490ea2e16160b7cca5c44a80c82
Taylor Smith <smith.taylord@gmail.com>
2023-04-07 09:21:24 -0600

    Moved hard coded items to the App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,33 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js'
 import SearchBar from '../SearchBar/SearchBar.js'
 
+let business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+let businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
 
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
