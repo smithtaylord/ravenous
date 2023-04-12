bd9f4398b14e8c6fac6fb84f87b860ebf32850c2
Taylor Smith <smith.taylord@gmail.com>
2023-04-12 08:55:07 -0600

    Application done!

--- src/Util/Yelp.js
+++ src/Util/Yelp.js
@@ -1,34 +1,35 @@
 const apiKey = '3gmu5uVRP_uikdQaLqN1FwE4d0ErGW51-X68trWp6dVfgg9fH6b8XWrWzuU2Xn90WT70mmCaCqyt7yVTRsQjl3VlIhxve-hHjWZCEk0QyctyR2JJhPZ12MFHsoA1ZHYx'
 
-let Yelp = {
+const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${apiKey}`
-                }
-            }).then(response => {
-                return response.json()
-            }).then(jsonResponse => {
-                if (jsonResponse.business) {
-                    return jsonResponse.business.map(business => {
-                        return {
-                            id: business.id,
-                            imageSrc: business.imageSrc,
-                            name: business.name,
-                            address: business.address,
-                            city: business.city,
-                            state: business.state,
-                            zipCode: business.zipCode,
-                            category: business.category,
-                            rating: business.rating,
-                            reviewCount: business.reviewCount
-                        }
-                    })
-                }
-            });
+        const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+        return fetch(url, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    };
+                });
+            }
+        });
     }
-}
+};
 
-export default Yelp
+export default Yelp;
+
 
